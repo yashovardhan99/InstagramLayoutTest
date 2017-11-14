621a5a7555948ae7811ea2e72835b675696a8a8b
yashovardhan99 <yashovardhandhanania@outlook.in>
2017-11-14 13:29:41 +0530

    Added PhoneAuthVerification in Testing state - completing commit

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.instagram;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
